8155719cc9ed11bf8ff30a8187ddf36ae525a64e
ShchipskiiYura <shchipskii@gmail.com>
2024-01-16 09:06:32 +0200

    fx android convert frame to baste64

--- android/src/main/java/com/visioncamerabase64plugin/VisionCameraBase64Plugin.java
+++ android/src/main/java/com/visioncamerabase64plugin/VisionCameraBase64Plugin.java
@@ -6,14 +6,8 @@ import com.mrousavy.camera.frameprocessor.Frame;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
 import java.util.Map;
 
-import com.google.mlkit.vision.common.InputImage;
-
 import android.media.Image;
 import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
-import android.util.Base64;
-import androidx.camera.core.ImageProxy;
-import java.io.ByteArrayOutputStream;
 
 
 public class VisionCameraBase64Plugin extends FrameProcessorPlugin {
@@ -22,24 +16,7 @@ public class VisionCameraBase64Plugin extends FrameProcessorPlugin {
   @Override
   public Object callback(@NonNull Frame frame, @Nullable Map<String, Object> arguments) {
     // TODO: image format and quality must come from params
-    Bitmap.CompressFormat imageFormat = Bitmap.CompressFormat.PNG;
-    int quality = 100;
-
-    Image mediaImage = frame.getImage();
-
-    InputImage image = InputImage.fromMediaImage(mediaImage, BitmapUtils.convertRotationDegreeFromString(frame.getOrientation()));
-
-    Bitmap bitmap = BitmapUtils.convertImageToBitmap(image);
-    return bitmapToBase64(bitmap, imageFormat, quality);
-  }
-
-  /** Converts a bitmap to base64 format string */
-  public static String bitmapToBase64(Bitmap bitmap, Bitmap.CompressFormat format, int quality)
-  {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    bitmap.compress(format, quality, outputStream);
-
-    return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+    return BitmapUtils.convertYuvToRgba(frame.getImage());
   }
 
   VisionCameraBase64Plugin(@Nullable Map<String, Object> options) {
